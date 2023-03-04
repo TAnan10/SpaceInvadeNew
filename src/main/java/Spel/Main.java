b45cd9ac27ef1db44a5d73af5291c3d07df3f1ee
erkan <eriol394@student.liu.se>
2023-03-04 18:17:08 +0100

    Version 2

--- src/main/java/Spel/Main.java
+++ src/main/java/Spel/Main.java
@@ -2,7 +2,7 @@ package Spel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the adventure  testtest ");
+        System.out.println("Welcome to the adventure  testtest A");
     }
 }
 
