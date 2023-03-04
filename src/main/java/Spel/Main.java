726075da2e77c34bde169f794378c565e64fd327
erkan <eriol394@student.liu.se>
2023-03-04 18:08:43 +0100

    commitq1

--- src/main/java/Spel/Main.java
+++ src/main/java/Spel/Main.java
@@ -2,7 +2,7 @@ package Spel;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the adventure game");
+        System.out.println("Welcome to the adventure");
     }
 }
 
