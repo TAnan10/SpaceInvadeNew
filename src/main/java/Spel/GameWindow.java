3733a9b88d5eea72b3ff1fcc3ea3bfdb49fc4b00
46727(Anan) <tusharahamedanan10@gmail.com>
2023-03-07 12:53:41 +0100

    Spel: Version 1

--- src/main/java/Spel/GameWindow.java
+++ src/main/java/Spel/GameWindow.java
@@ -1,15 +1,22 @@
 package Spel;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 
-public class GameWindow extends JFrame {
+public class GameWindow {
     private JFrame jframe;
 
     public GameWindow(GamePanel gamePanel) {
+
         jframe = new JFrame();
-        jframe.setSize(400, 400); //The size of the game window (Width, Height)
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Closes the program when you close the window
-        jframe.add(gamePanel); //Adds the panel onto the frame, makes what you drew on the panel visible in the frame
-        jframe.setVisible(true); //Makes the gamewindow visible (Has to be at the bottom of the code)
+
+        jframe.setSize(600, 600);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.add(gamePanel);
+        jframe.setLocationRelativeTo(null);
+        jframe.setVisible(true);
+
     }
+
 }
+
+
